00b2932e1405c8b08d616c67208ddda71f00225c
ap-justin <justin@angelprotocol.io>
2022-08-22 15:49:00 +0800

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./app";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+    <BrowserRouter basename="/querier">
+      <Routes>
+        <Route path={"/*"} element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
