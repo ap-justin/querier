4278a1fd78c59d8b58ab4a59d088364f67b498ba
ap-justin <justin@angelprotocol.io>
2022-08-31 15:22:35 +0800

    use hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
-import App from "./app";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path={"/*"} element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
